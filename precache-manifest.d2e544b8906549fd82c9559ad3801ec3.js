d6993db82d405a124eaad25ac2980be9a1761d12
Reginald Galang <reguv760@gmail.com>
2019-03-29 09:27:25 -0700

    Updates

---
+++ precache-manifest.d2e544b8906549fd82c9559ad3801ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82c36fb6d19137cb8b75",
+    "url": "/recipe-app/static/js/runtime~main.11ea1cab.js"
+  },
+  {
+    "revision": "0582b6be6c10e3e18b3e",
+    "url": "/recipe-app/static/js/main.c130e3d7.chunk.js"
+  },
+  {
+    "revision": "0c611bb2f0fc8415bf80",
+    "url": "/recipe-app/static/js/2.915579d8.chunk.js"
+  },
+  {
+    "revision": "0582b6be6c10e3e18b3e",
+    "url": "/recipe-app/static/css/main.28e0782c.chunk.css"
+  },
+  {
+    "revision": "0c611bb2f0fc8415bf80",
+    "url": "/recipe-app/static/css/2.d52f845f.chunk.css"
+  },
+  {
+    "revision": "7d4dc0261fa99428f3adb30f7bdc9946",
+    "url": "/recipe-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a057c2919f2fbe0de947adc1750b6f89.js"
+  "/recipe-app/precache-manifest.d2e544b8906549fd82c9559ad3801ec3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/recipe-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
